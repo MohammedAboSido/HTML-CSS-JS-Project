c481d98a16c6e21a7f0f56ad8eab77444568de93
Mohammed A. Abo Sido <abosido2@gmail.com>
2022-08-16 13:07:33 +0300

    Video 7

--- js/master.js
+++ js/master.js
@@ -1,3 +1,13 @@
+//Toggle Spin Class On Icon 
+document.querySelector( ".setting-icon .fa-cog" ).onclick = function ()
+{
+    //Toggle Class Fa-spin For Rotation On Self
+    this.classList.toggle( "fa-spin" );
+    //Toggle Class Open On Main Settings Box
+    document.querySelector( ".settings-box" ).classList.toggle( "open" );
+};
+
+
 //Select Landing Page Element
 
 let landingPage = document.querySelector( ".landing-page" );
