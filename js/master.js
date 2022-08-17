c5f2e49216fe043b254b468f91630b2ad04e0e14
Mohammed A. Abo Sido <abosido2@gmail.com>
2022-08-17 20:51:38 +0300

    From Video 12 To 24

--- js/master.js
+++ js/master.js
@@ -147,4 +147,95 @@ function randomizeImgs ()
 }
 
 
+//Select Skills Selector
+let ourSkills = document.querySelector( ".skills" );
+
+window.onscroll = function ()
+{
+    //skills Offset top
+    let skillsOffsetTop = ourSkills.offsetTop;
+    //Skills Outer Height
+    let skillsOuterHeight = ourSkills.offsetHeight;
+
+    //Window Height 
+    let windowHeight = this.innerHeight;
+
+    //window scrollTop
+    let windowScrollTop = this.pageYOffset;
+
+    if ( windowScrollTop > ( ( skillsOffsetTop + skillsOuterHeight ) - windowHeight ) )
+    {
+        let allSkills = document.querySelectorAll( ".skill-box .skill-progress span" );
+        allSkills.forEach( skill =>
+        {
+            skill.style.width = skill.dataset.progress;
+        } );
+    }
+};
+
+//Create Popup With The Image
+
+let ourGallery = document.querySelectorAll( ".gallery .images-box img" );
+
+ourGallery.forEach( img =>
+{
+    img.addEventListener( 'click', ( e ) =>
+    {
+        //create Overlay Element 
+        let overlay = document.createElement( "div" );
+        //Add Class To Overlay
+        overlay.classList.add( "popup-overlay" );
+        //Append  Overlay To body
+        document.body.appendChild( overlay );
+        //Create Popup
+        let popupBox = document.createElement( "div" );
+        //Add Class To Popup Box 
+        popupBox.className = "popup-box";
+
+        if ( img.alt !== null )
+        {
+            //create Heading
+            let imgHeading = document.createElement( "h3" );
+            //Create Text For Heading 
+            let imgText = document.createTextNode( img.alt );
+            //Append Text To The Heading 
+            imgHeading.appendChild( imgText );
+            //Append The Heading To Popup Box
+            popupBox.appendChild( imgHeading );
+        }
+
+        //Create The Img 
+        let popupImage = document.createElement( "img" );
+        //Set Img Source 
+        popupImage.src = img.src;
+        //Add Img To Popup Box 
+        popupBox.appendChild( popupImage );
+        document.body.appendChild( popupBox );
+
+        //Create The Close Span 
+        let closeButton = document.createElement( "span" );
+        //Create The Close Button Text 
+        let closeButtonText = document.createTextNode( "X" );
+        //Append Text To Close Button 
+        closeButton.appendChild( closeButtonText );
+        //Add Class To Close Button 
+        closeButton.className = "close-button";
+        //Add Close Button To Popup Box 
+        popupBox.appendChild( closeButton );
+
+    } );
+} );
+
+//Close Popup 
+document.addEventListener( "click", function ( e )
+{
+    if ( e.target.className === "close-button" )
+    {
+        //Remove The Current Popup
+        e.target.parentElement.remove();
+        //Remove The Overlay
+        document.querySelector( ".popup-overlay" ).remove();
+    }
+
+} );
 
